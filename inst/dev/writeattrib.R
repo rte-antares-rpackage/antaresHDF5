7f7fd44f05dc095ff632edfb2422d4f3e4cd2ab2
TitouanRobert <titouan.robert.pro@gmail.com>
2017-06-19 17:46:34 +0200

    affectation attributs

--- inst/dev/writeattrib.R
+++ inst/dev/writeattrib.R
@@ -2,10 +2,10 @@ source("inst/dev/writeAttribFunction.R")
 library(data.table)
 library(antaresRead)
 library(rhdf5)
+library(pipeR)
+
 setSimulationPath("D:/exemple_test", "SimulForH5")
 res <- readAntares(areas = "all", links = "all", clusters = "all", districts = "all", mcYears = "all")
-
-
 
 attrib <- attributes(res)
 H5close()
@@ -21,8 +21,13 @@ class(attribList2$opts) <- "simOptions"
 Errors <- testIdentical(attribList2,attrib )
 Errors <- unlist(Errors)[!unlist(Errors)]
 Errors
-
-
+attributes(res) <- NULL
+sapply(1:length(attribList2), function(X){
+  attr(res, names(attribList2)[X]) <- attribList2[[X]]
+  NULL
+}) %>>% invisible()
+res
+str(res)
 
 
 
