7b29dd0bcf33aadcd244c324153a5faace20c1fc
TitouanRobert <titouan.robert.pro@gmail.com>
2017-10-09 09:44:42 +0200

    remove parrallel test

--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -1,18 +1,18 @@
 context("Write h5")
  
-tptpDir <- paste0(tpDir, "/tpDir")
-dir.create(tptpDir)
-
-test_that("write more than one studies mono thread", {
-writeAntaresH5(path = tptpDir, 
-               timeSteps = "annual", nbCores = 1)
-  
-})
-unlink(tptpDir, recursive = TRUE)
-dir.create(tptpDir)
-
-test_that("write more than one studies multi thread", {
-  writeAntaresH5(path = tptpDir, 
-                 timeSteps = "annual", nbCores = 2)
-})
-unlink(tptpDir, recursive = TRUE)
+# tptpDir <- paste0(tpDir, "/tpDir")
+# dir.create(tptpDir)
+# 
+# test_that("write more than one studies mono thread", {
+# writeAntaresH5(path = tptpDir, 
+#                timeSteps = "annual", nbCores = 1)
+#   
+# })
+# unlink(tptpDir, recursive = TRUE)
+# dir.create(tptpDir)
+# 
+# test_that("write more than one studies multi thread", {
+#   writeAntaresH5(path = tptpDir, 
+#                  timeSteps = "annual", nbCores = 2)
+# })
+# unlink(tptpDir, recursive = TRUE)
