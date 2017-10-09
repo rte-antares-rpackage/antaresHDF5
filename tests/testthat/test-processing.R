62725e7b4f3a2a6d6626c94837c3754857159034
TitouanRobert <titouan.robert.pro@gmail.com>
2017-10-09 17:53:46 +0200

    processing test

--- tests/testthat/test-processing.R
+++ tests/testthat/test-processing.R
@@ -14,9 +14,22 @@ test_that("processing", {
                  surplus = TRUE,
                  surplusClusters = TRUE,
                  evalAreas = list(Tota = "`H. STOR` + `MISC. DTG`",
-                                  Tota2 = "NODU + `NP COST` + 1"),
+                                  Tota2 = "`NODU` + `NP COST` + 1"),
                  evalLinks = list(),
                  evalClusters = list(),
-                 evalDistricts = list(),
-                 columnsToSelects = c("H. STOR", " MISC. DTG", "NODU", "NP COST"))
+                 evalDistricts = list())
+  
+  calcData <- readAntares(areas = "all", mcYears = "all", select = c("H. STOR" , "MISC. DTG",
+                                                                     "NODU" , "NP COST",
+                                                                     "Tota", "Tota2"))
+  
+  
+  calcData[,verif1 := `H. STOR` + `MISC. DTG`]
+  calcData[,verif2 := `NODU` + `NP COST` + 1]
+  expect_true(max(calcData$Tota-calcData$verif1) == 0)
+  expect_true(max(calcData$Tota2-calcData$verif2) == 0)
+  
+  
+  
+  
 })
